1fd49f99c4ec7c2f8f88406f00fbbd688622c5de
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-02-10 15:47:48 -0500

    structured todo form with handlers

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TodoForm extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
-}
+const TodoForm = ({handleSubmit, handleChanges, newTask, handleClear}) => {
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type='text' name='newTask' value={newTask} onChange={handleChanges}/>
+            <button type='submit'>Add Todo</button>
+            <button onClick={handleClear}>Clear Completed</button>
+        </form>
+    );
+};
 
 export default TodoForm;
